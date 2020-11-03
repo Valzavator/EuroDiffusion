a36fed7871f6dbe665e3f00505bcbdecc85aa346
Maksym Svynarchuk <maksym_svynarchuk@epam.com>
2020-11-03 21:11:58 +0200

    Refactoring OutputCountryDataComparator

--- src/main/java/com/gmail/maxsmv1998/eurodiffusion/util/comporator/OutputCountryDataComparator.java
+++ src/main/java/com/gmail/maxsmv1998/eurodiffusion/util/comporator/OutputCountryDataComparator.java
@@ -8,9 +8,8 @@ public class OutputCountryDataComparator implements Comparator<OutputCountryData
 
     @Override
     public int compare(OutputCountryData o1, OutputCountryData o2) {
-        int amountOfDaysCompared = Integer.compare(o1.getAmountOfDays(), o2.getAmountOfDays());
-        return amountOfDaysCompared == 0
+        return o1.getAmountOfDays() == o2.getAmountOfDays()
                 ? o1.getName().compareTo(o2.getName())
-                : amountOfDaysCompared;
+                : Integer.compare(o1.getAmountOfDays(), o2.getAmountOfDays());
     }
 }
